8b479d62ed1907666b81a599c6ef9bd3d9ced0a7
Valentine V <valentinvoskoboinik@rambler.ru>
2014-05-08 19:39:05 +0400

    Add gitignore remaininng modemodules folder

--- nodeServer.js
+++ nodeServer.js
@@ -1,9 +1,44 @@
 var http = require("http");
-var socket = require("socket.io");
+var url = require('url');
 var fs = require("fs");
+var socketio = require("socket.io");
 
-var server = http.createServer();
+var server = http.createServer(function(request,response){
+	var path = url.parse(request.url).pathname;
+	switch(path){
+        case '/':
+			response.writeHead(200, {'Content-Type': 'text/html',"X-Head": "Node-Server"});
+            response.write('hello world');
+			response.end();
+            break;
+        case '/testApp.html':
+            fs.readFile('D:/webRtcProj/testApp.html', function(error, data){
+                if(error !== null){
+                    response.writeHead(404, {'Content-Type': 'text/html'});
+                    response.write("read file opps this doesn't exist - 404");
+                }else{
+                    response.writeHead(200, {"Content-Type": "text/html","X-Head": "Node-Server"});
+                    response.write(data, "utf8");
+					response.end();
+                }
+            });
+            break;
+        default:
+            response.writeHead(404, {'Content-Type': 'text/html'});
+            response.write("opps this doesn't exist - 404");
+			response.end();
+            break;
+    }
+});
 
-var io =  socket.listen(server);
+server.listen(8888);
 
-io.listen(8888);
+var io = socketio.listen(server);
+
+io.sockets.on("connection", function(socket){
+	console.log("IO connection");
+	socket.emit('handshake',{message : "Connection established"});
+});
+
+console.log("Node server is running.");
+
