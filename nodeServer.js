db5011791c04bca6c9ea4027923ffcdf8b13e96d
Valentine V <valentinvoskoboinik@rambler.ru>
2014-05-12 12:34:12 +0400

    create server routing part, rename testApp to index, add test file, add sending offer

--- nodeServer.js
+++ nodeServer.js
@@ -2,33 +2,107 @@ var http = require("http");
 var url = require('url');
 var fs = require("fs");
 var socketio = require("socket.io");
+var serverRoot = "D:/webRtcProj";
+var defaultFile = "index.html";
+
+var mimes = {
+	js : "appication/javascript",
+	html : "text/html",
+	css : "text/css",
+	gif : "image/gif",
+	png : "image/png",
+	jpeg : "image/jpeg",
+	jpg : "image/jpg",
+}
+
+function checkMime(path, mimesSupported){
+	var lastIndex = path.lastIndexOf(".");
+	var mime = path.substring(lastIndex + 1, path.length);
+	if(mimesSupported[mime] !== undefined){
+		return mimesSupported[mime];
+	}else{
+		return "undefined-mime/unedfined";
+	}
+}
+
+function combinePath(path){
+	if(path === "/")
+		return "/";
+	var pathParts = path.split("/");
+	var pathPartsCleared = [];
+	for(var part in pathParts){
+		if(pathParts[part] !== ""){	
+			switch(pathParts[part]){
+				case "..":
+					pathPartsCleared.pop()
+					break
+				case ".":
+					continue
+					break
+				default:
+					pathPartsCleared.push(pathParts[part])
+					break
+			}
+		}	
+	}
+
+	var file = pathPartsCleared.pop();
+	var path = "/" + pathPartsCleared.join("/") + "/" + file;
+	return path;
+}
 
 var server = http.createServer(function(request,response){
 	var path = url.parse(request.url).pathname;
-	switch(path){
+	
+	var pathCombined = combinePath(path);
+
+	fs.readFile(serverRoot + pathCombined, function(error, data){
+		if(error !== null){
+			fs.readFile(serverRoot + pathCombined + "/index.html", function(error, data){
+				if(error !== null){
+					console.log("cannot provide file " + pathCombined);
+					response.writeHead(404, {'Content-Type': 'text/html'});
+                    response.write("read file opps this doesn't exist - 404");
+					response.end();
+				}else{
+					response.writeHead(200, {"Content-Type": "text/html"});
+					response.write(data, "utf8");
+					response.end();
+				}
+			})
+		}else{
+			var mime = checkMime(pathCombined, mimes);
+			response.writeHead(200, {"Content-Type": mime});
+            response.write(data, "utf8");
+			response.end();
+		}
+	})
+	
+	/*switch(path){
         case '/':
-			response.writeHead(200, {'Content-Type': 'text/html',"X-Head": "Node-Server"});
+			response.writeHead(200, {'Content-Type': 'text/html'});
             response.write('hello world');
 			response.end();
             break;
         case '/testApp.html':
-            fs.readFile('D:/webRtcProj/testApp.html', function(error, data){
+            fs.readFile(serverRoot + '/testApp.html', function(error, data){
                 if(error !== null){
                     response.writeHead(404, {'Content-Type': 'text/html'});
                     response.write("read file opps this doesn't exist - 404");
                 }else{
-                    response.writeHead(200, {"Content-Type": "text/html","X-Head": "Node-Server"});
+                    response.writeHead(200, {"Content-Type": "text/html"});
                     response.write(data, "utf8");
 					response.end();
                 }
             });
             break;
         default:
+			//console.log("cannot provide file " + path);
             response.writeHead(404, {'Content-Type': 'text/html'});
             response.write("opps this doesn't exist - 404");
 			response.end();
             break;
-    }
+    }*/
 });
 
 server.listen(8888);
